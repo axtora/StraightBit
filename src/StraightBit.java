09bc8154a0f987f988b63b4bfe2fb7cf7402675f
Maestro <axtora@naver.com>
2019-02-15 21:17:37 +0900

    beta

---
+++ src/StraightBit.java
@@ -0,0 +1,70 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import character.move;
+
+
+public class StraightBit extends JFrame {
+	Image screen;
+	Graphics screen_image;
+	
+	Image character_circle = new ImageIcon(index.class.getResource("Image/circle.png")).getImage();
+	
+	int charX=0,charY=0;
+	
+	public StraightBit() {
+		setSize(index.SCREEN_WIDTH,index.SCREEN_HEIGHT);
+		setTitle("StraightBit");
+		
+		move mv = new move();
+	
+		
+		Color bg = new Color(75, 75, 75);
+		
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setLayout(null);
+		setBackground(bg);
+		
+		Music music = new Music("Keep_You.mp3", true);
+		music.start();
+		
+		Thread th = new Thread();
+		
+		while(true) {
+			charY ++;
+			try {
+				th.sleep(10);
+				System.out.println(charY);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+
+	
+	@Override
+	public void paint(Graphics g) {
+		screen = createImage(1280,720);			//가로 * 세로 크기의 이미지 생성(도화지)
+		screen_image = screen.getGraphics();  //이미지의 그래픽을 얻어옴
+		screenDraw(screen_image);			
+		g.drawImage(screen, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		
+		g.drawImage(character_circle, 0, charY, null);	//얻어온 그래픽을 화면에 출력
+		this.repaint();			//paint 반복
+	}
+	
+}
---
+++ src/character/move.java
@@ -0,0 +1,39 @@
+package character;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Graphics;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+
+public class move extends KeyAdapter {
+	Image character = new ImageIcon(getClass().getResource("../Image/circle.png")).getImage();
+	
+	int x, y;
+	
+	public void moving(Graphics g) {
+		System.out.println("moving method in");
+		g.drawImage(character, x, y, null);
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+
+		switch (key) {
+		case KeyEvent.VK_LEFT:
+			x = x - 10;
+			System.out.println("right Click");
+			break;
+
+		case KeyEvent.VK_RIGHT:
+			y = y + 10;
+			System.out.println("left Click");
+			break;
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("move.java in");
+	}
+}
